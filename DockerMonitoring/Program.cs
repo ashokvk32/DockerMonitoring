7cb14de4deac5fc0ef28addb2c1f24f86e6b6069
Ashok Karunakaran <ashok.vk.32@gmail.com>
2019-06-16 15:04:51 -0400

    Writing docker perf data to elasticsearch 	modified:   DockerMonitoring/DockerMonitoring.csproj 	modified:   DockerMonitoring/Program.cs 	modified:   DockerMonitoring/bin/Debug/DockerMonitoring.exe 	modified:   DockerMonitoring/bin/Debug/DockerMonitoring.pdb 	modified:   DockerMonitoring/obj/Debug/DockerMonitoring.csproj.CoreCompileInputs.cache 	modified:   DockerMonitoring/obj/Debug/DockerMonitoring.exe 	modified:   DockerMonitoring/obj/Debug/DockerMonitoring.pdb 	.vs/ 	DockerMonitoring/DockerConnect.cs 	DockerMonitoring/Dtos/ 	DockerMonitoring/ElasticsearchClient.cs 	DockerMonitoring/bin/Debug/Elasticsearch.Net.dll 	DockerMonitoring/bin/Debug/Elasticsearch.Net.xml 	DockerMonitoring/bin/Debug/Nest.dll 	DockerMonitoring/bin/Debug/Nest.xml 	packages/Elasticsearch.Net.6.8.0/ 	packages/NEST.6.8.0/

--- DockerMonitoring/Program.cs
+++ DockerMonitoring/Program.cs
@@ -5,6 +5,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Docker.DotNet;
 using Docker.DotNet.Models;
+using DockerMonitoring.Dtos;
+using Nest;
 
 namespace DockerMonitoring
 {
@@ -15,81 +17,45 @@ namespace DockerMonitoring
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the IP of Ubuntu/ Windows machine with remote docker config enabled");
-            var machineIp = Console.ReadLine();
-            Console.WriteLine("Enter the port that Docker listens to");
-            var machinePort = Console.ReadLine();
-            try
+//            var node = new Uri("http://localhost:9200");
+//
+//            var settings = new ConnectionSettings(node).DefaultIndex("testindex1234");
+//
+//            var client = new ElasticClient(settings);
+//            var player = new Player
+//            {
+//                Name = "Zanetti",
+//                Club = "Inter"
+//            };
+//            if (!client.IndexExists("testindex1234").Exists)
+//            {
+//                var response = client.CreateIndex("testindex1234");
+//            }
+            //var response = client.IndexDocument(player);
+            DockerConnect connect = new DockerConnect();
+            connect.ConnectToDocker();
+
+            ElasticsearchClient esClient = new ElasticsearchClient();
+            var response = esClient.Search("dockerweb");
+            foreach (var perfData in response)
             {
-                if (!string.IsNullOrEmpty(machineIp) && !string.IsNullOrEmpty(machinePort))
-                {
-                    var uri = new Uri($"http://{machineIp}:{machinePort}");
-                    var dockerClient = new DockerClientConfiguration(uri).CreateClient();
-                    var containers = GetContainers(dockerClient).Result;
-                    Console.WriteLine("Select the docker container to monitor:");
-                    containers.ForEach(x => Console.WriteLine(x.Names.First()));
-
-                    var containerName = Console.ReadLine();
-                    GetContainerStats(dockerClient, containerName).Wait();
-
-                    Console.ReadLine();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
+                Console.WriteLine(perfData.Timestamp);
             }
 
-        }
-
-        /// <summary>
-        /// Get a list of containers for a docker environment
-        /// </summary>
-        /// <param name="client">Instance of a docker environment</param>
-        /// <returns>List of containers in the docker</returns>
-        private static async Task<List<ContainerListResponse>> GetContainers(IDockerClient client)
-        {
-            // Limit the number of containers to 10
-            var containers = await client.Containers.ListContainersAsync(
-                new ContainersListParameters()
-                {
-                    Limit = 10
-                });
-            // Print the names of the containers
-            return containers.ToList();
-        }
-
-        /// <summary>
-        /// Get the performance stats for a container
-        /// </summary>
-        /// <param name="client">Docker environment instance</param>
-        /// <param name="containerName">Name of the container</param>
-        /// <returns></returns>
-        private static async Task GetContainerStats(IDockerClient client, string containerName)
-        {
-            containerName = containerName.Trim('/');
-            Console.WriteLine("Getting performance data for container {0}:", containerName);
-            //Setting to stream to false returns only one data point. Setting this to yes returns data at continuous intervals.
-            var statsParameters = new ContainerStatsParameters { Stream = false };
-
-            IProgress<ContainerStatsResponse> progress =
-                new Progress<ContainerStatsResponse>(GetPerformanceStats);
-
-            await client.Containers.GetContainerStatsAsync(containerName, statsParameters, progress, CancellationToken.None);
+            
+            Console.ReadLine();
 
         }
 
-        /// <summary>
-        /// Output the statistics
-        /// </summary>
-        /// <param name="stats">Statistics for the container</param>
-        private static void GetPerformanceStats(ContainerStatsResponse stats)
-        {
-            Console.WriteLine($"Timestamp = {stats?.Read}, " +
-                                  $"CpuUsage = {stats?.PreCPUStats?.CPUUsage?.TotalUsage}, " +
-                                  $"MemoryUsage = {stats?.MemoryStats?.Usage}, " +
-                                    $"ReceivedBytes = {stats?.Networks?.FirstOrDefault().Value?.RxBytes}");
-        }
+        
+        
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public string Club { get; set; }
+
     }
 }
